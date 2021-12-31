682c39b51c3ff11d4cd68b5889c871e1ebfb3bf0
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2021-12-31 20:36:08 +0600

    Add files via upload

---
+++ BEE 1070.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n=0,i;
+    cin>>n;
+    for(i=n;i<n+12;i++)
+        if(i%2!=0)
+
+        cout<<i<<endl;
+        return 0;
+}
+//54259
