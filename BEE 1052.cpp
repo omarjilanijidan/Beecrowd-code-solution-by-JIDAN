50ce28710195520c9505ffbf0321366c67210f68
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2021-12-31 17:20:47 +0600

    Add files via upload

---
+++ BEE 1052.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    string month[]={"\0","January","February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+    cin>>n;
+    cout<<month[n]<<endl;
+
+    return 0;
+}
