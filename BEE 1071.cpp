a357911482111983c3420a83793c3ab72abcad01
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-01 22:42:05 +0600

    Add files via upload

---
+++ BEE 1071.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,x,y,i;
+
+    while(cin>>x>>y)
+    {
+
+        if(x>y)
+
+        {
+            int sum=0;
+            for(i=y;i<x;i++)
+
+            {
+                if(i%2!=0 && i!=y)
+                    sum=sum+i;
+            }
+            cout<<sum<<endl;
+        break;
+
+        }
+        else if(y>x)
+
+        {
+            int sum=0;
+            for(i=x;i<y;i++)
+            {
+                if(i%2!=0 && i!=x)
+                    sum=sum+i;
+            }
+            cout<<sum<<endl;
+        break;
+
+        }
+        else if(y==x)
+
+        {
+            cout<<x-y<<endl;
+        }
+        break;
+    }
+    return 0;
+}
