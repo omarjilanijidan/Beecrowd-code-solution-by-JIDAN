cb1efb1897b8f20a2983585ab0b138b45b356477
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-02 20:22:03 +0600

    Add files via upload

---
+++ BEE 1078.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i,j;
+    cin>>n;
+    for(i=1,j=1;i<=10;i++)
+    {
+
+        j=i*n;
+
+        cout<<i<<" x "<<n<<" = "<<j<<endl;
+    }
+    return 0;
+
+}
+
---
+++ BEE 1095.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int i,j;
+    for(i=1,j=60;j>=0;i=i+3,j=j-5)
+        {
+            cout<<"I="<<i<<" "<<"J="<<j<<endl;
+
+
+        }
+
+    return 0;
+}
