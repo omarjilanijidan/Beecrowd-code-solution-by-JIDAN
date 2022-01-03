4aece2c7a6b7644deccfe0b8520ef9d70dd935c1
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-03 20:56:50 +0600

    Add files via upload

---
+++ BEE 1866.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int c,i=1,s=0,n;
+    cin>>c;
+    while(c-->0)
+    {
+        cin>>n;
+        if(n%2==0)
+            cout<<"0"<<endl;
+        else
+            cout<<"1"<<endl;
+
+
+    }
+    return 0;
+
+}
