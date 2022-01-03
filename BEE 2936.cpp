6e2b4462ea2279b39b75dbe7bdec53ed40b1dc08
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-03 21:45:55 +0600

    Add files via upload

---
+++ BEE 2936.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c,d,e,total;
+    cin>>a>>b>>c>>d>>e;
+    total=((a*300)+(b*1500)+(c*600)+(d*1000)+(e*150))+225;
+
+
+    cout<<total<<endl;
+    return 0;
+}
