29d440cfc6d3074c35b5c12990a15ab3f6133fb1
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-03 23:47:26 +0600

    Add files via upload

---
+++ BEE 1924.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,i;
+    string jidan;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+         getline(cin,jidan);
+    }
+    cout<<"Ciencia da Computacao"<<endl;
+    return 0;
+
+}
