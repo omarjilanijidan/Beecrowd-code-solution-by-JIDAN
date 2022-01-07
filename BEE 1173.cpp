8bf5e4eb513dc37e352d121d4af47a5affdb70d6
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-07 17:25:00 +0600

    Add files via upload

---
+++ BEE 1173.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n=10,i,mx=0,pos=0,j;
+    int arr[10];
+    cin>>j;
+
+    for(i=0;i<n;i++)
+    {
+        cout<<"N["<<i<<"] = "<<j<<endl;
+        j=j*2;
+
+    }
+    return 0;
+
+}
+
