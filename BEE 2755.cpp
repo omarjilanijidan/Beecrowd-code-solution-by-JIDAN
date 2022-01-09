45659feea7897df26bb0bff62cf3b3fde56b0ec6
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-09 18:53:03 +0600

    Add files via upload

---
+++ BEE 2755.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    cout<<"\"Ro'b'er\tto\\/\""<<endl;
+    cout<<"(._.) ( l: ) ( .-. ) ( :l ) (._.)"<<endl;
+    cout<<"(^_-) (-_-) (-_^)"<<endl;
+    cout<<"(\"_\") ('.')"<<endl;
+
+
+    return 0;
+}
