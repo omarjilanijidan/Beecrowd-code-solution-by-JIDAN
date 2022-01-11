7a44194f287753c27efa2e83e816dd4011837401
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-11 21:04:01 +0600

    Add files via upload

---
+++ BEE 2759.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    char x,y,z;
+    cin>>x>>y>>z;
+    cout<<"A = "<<x<<", B = "<<y<<", C = "<<z<<endl;
+    cout<<"A = "<<y<<", B = "<<z<<", C = "<<x<<endl;
+    cout<<"A = "<<z<<", B = "<<x<<", C = "<<y<<endl;
+    return 0;
+}
