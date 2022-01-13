322d5a7cb9200dd5c4706e931fa7633f6f9d837f
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-14 00:05:03 +0600

    Add files via upload

---
+++ BEE 1181.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int L;
+    cin>>L;
+    char A='S',B='M',O;
+    cin>>O;
+    int i , j , count=0;
+    double arr[12][12],sum=0;
+
+
+    for(i=0;i<12;i++)
+    {
+        for(j=0;j<12;j++)
+            cin>>arr[i][j];
+    }
+
+    for(i=L,j=0;j<12;j++,count++)
+        sum=sum+arr[i][j];
+
+    cout<<std::fixed;
+    cout<<setprecision(1);
+    if(O==A)
+        cout<<sum<<endl;
+    else if(O==B)
+        cout<<sum/count<<endl;
+    return 0;
+}
