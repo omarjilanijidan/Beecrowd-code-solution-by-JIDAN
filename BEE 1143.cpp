ff650d095f167cbf22af70fba8f11081192d3082
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-14 11:09:29 +0600

    Add files via upload

---
+++ BEE 1143.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x,i;
+    cin>>n;
+
+
+    for(i=1;i<=n;i++)
+        cout<<i<<" "<<i*i<<" "<<i*i*i<<endl;
+    return 0;
+
+}
