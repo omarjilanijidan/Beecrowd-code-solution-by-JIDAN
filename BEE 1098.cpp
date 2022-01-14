45efcb611df4c3a9d90465bb0988865459517aaf
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-14 11:48:05 +0600

    Add files via upload

---
+++ BEE 1098.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"I=0 J=1"<<endl;
+    cout<<"I=0 J=2"<<endl;
+    cout<<"I=0 J=3"<<endl;
+    cout<<"I=0.2 J=1.2"<<endl;
+    cout<<"I=0.2 J=2.2"<<endl;
+    cout<<"I=0.2 J=3.2"<<endl;
+    cout<<"I=0.4 J=1.4"<<endl;
+    cout<<"I=0.4 J=2.4"<<endl;
+    cout<<"I=0.4 J=3.4"<<endl;
+    cout<<"I=0.6 J=1.6"<<endl;
+    cout<<"I=0.6 J=2.6"<<endl;
+    cout<<"I=0.6 J=3.6"<<endl;
+    cout<<"I=0.8 J=1.8"<<endl;
+    cout<<"I=0.8 J=2.8"<<endl;
+    cout<<"I=0.8 J=3.8"<<endl;
+    cout<<"I=1 J=2"<<endl;
+    cout<<"I=1 J=3"<<endl;
+    cout<<"I=1 J=4"<<endl;
+    cout<<"I=1.2 J=2.2"<<endl;
+    cout<<"I=1.2 J=3.2"<<endl;
+    cout<<"I=1.2 J=4.2"<<endl;
+    cout<<"I=1.4 J=2.4"<<endl;
+    cout<<"I=1.4 J=3.4"<<endl;
+    cout<<"I=1.4 J=4.4"<<endl;
+    cout<<"I=1.6 J=2.6"<<endl;
+    cout<<"I=1.6 J=3.6"<<endl;
+    cout<<"I=1.6 J=4.6"<<endl;
+    cout<<"I=1.8 J=2.8"<<endl;
+    cout<<"I=1.8 J=3.8"<<endl;
+    cout<<"I=1.8 J=4.8"<<endl;
+    cout<<"I=2 J=3"<<endl;
+    cout<<"I=2 J=4"<<endl;
+    cout<<"I=2 J=5"<<endl;
+
+
+
+
+    return 0;
+
+}
