54601543b1157f955231aab849dd1cda5e22e595
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-14 23:39:19 +0600

    Add files via upload

---
+++ BEE 2987.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    char L;
+    cin>>L;
+    char A='A', B='B', C='C', D='D', E='E', F='F', G='G', H='H', I='I',J='J';
+    char K='K', l='L', M='M', N='N', O='O', P='P', Q='Q', R='R', S='S', T='T';
+    char U='U',V='V',W='W', X='X', Y='Y', Z='Z';
+    if(L== A)
+        cout<<"1"<<endl;
+    else if(L== B)
+        cout<<"2"<<endl;
+    else if(L== C)
+        cout<<"3"<<endl;
+    else if(L== D)
+        cout<<"4"<<endl;
+    else if(L== E)
+        cout<<"5"<<endl;
+    else if(L== F)
+        cout<<"6"<<endl;
+    else if(L== G)
+        cout<<"7"<<endl;
+    else if(L== H)
+        cout<<"8"<<endl;
+    else if(L== I)
+        cout<<"9"<<endl;
+    else if(L== J)
+        cout<<"10"<<endl;
+    else if(L== K)
+        cout<<"11"<<endl;
+    else if(L== l)
+        cout<<"12"<<endl;
+    else if(L== M)
+        cout<<"13"<<endl;
+    else if(L== N)
+        cout<<"14"<<endl;
+    else if(L== O)
+        cout<<"15"<<endl;
+    else if(L== P)
+        cout<<"16"<<endl;
+    else if(L== Q)
+        cout<<"17"<<endl;
+    else if(L== R)
+        cout<<"18"<<endl;
+    else if(L== S)
+        cout<<"19"<<endl;
+    else if(L== T)
+        cout<<"20"<<endl;
+    else if(L== U)
+        cout<<"21"<<endl;
+    else if(L== V)
+        cout<<"22"<<endl;
+    else if(L== W)
+        cout<<"23"<<endl;
+    else if(L== X)
+        cout<<"24"<<endl;
+    else if(L== Y)
+        cout<<"25"<<endl;
+    else if(L== Z)
+        cout<<"26"<<endl;
+    return 0;
+
+
+}
