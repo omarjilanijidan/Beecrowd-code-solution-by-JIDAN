743e26e03b95e9808e282eb248f980648dc786fc
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-17 18:01:26 +0600

    Add files via upload

---
+++ BEE 2753.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int j;
+    char arr1[26]={'a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x','y','z'};
+    for(int i=0,j=97;i<26;i++,j++)
+        cout<<j<<" e "<<arr1[i]<<endl;
+    return 0;
+
+}
