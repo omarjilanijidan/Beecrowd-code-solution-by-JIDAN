f0c7e620e520cf4abdeae547e9abd87bd66399b0
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-18 13:22:26 +0600

    Add files via upload

---
+++ BEE 1564.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        if(n==0)
+            cout<<"vai ter copa!"<<endl;
+        else if(n>0)
+            cout<<"vai ter duas!"<<endl;
+    }
+    return 0;
+}
