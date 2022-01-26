c7601ad974807dd51ca8de7414381b7ae79e454e
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-26 23:55:12 +0600

    Add files via upload

---
+++ BEE 2581.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        string name;
+        getline(cin,name);
+        cout<<"I am Toorg!"<<endl;
+
+    }
+
+    return 0;
+}
