116e78c32218d02414f878b0298dca0920b0a959
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-26 20:09:31 +0600

    Add files via upload

---
+++ BEE 2234.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double m,n;
+    double div;
+    cin>>m>>n;
+    cout<<std::fixed;
+    cout<<setprecision(2);
+    div=m/n;
+    cout<<div<<endl;
+    return 0;
+}
