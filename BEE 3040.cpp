28502bd9931d256d7d08c03911f155cfc4104f32
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-28 22:15:13 +0600

    Add files via upload

---
+++ BEE 3040.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,h,d,g;
+    cin>>n;
+    while(n-->0)
+    {
+        cin>>h>>d>>g;
+        if(h>=200 && h<=300 && d>=50 && g>=150)
+        {
+            cout<<"Sim"<<endl;
+        }
+        else
+            cout<<"Nao"<<endl;
+    }
+    return 0;
+}
