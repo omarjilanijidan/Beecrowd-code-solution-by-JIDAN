41a9e4a4e236d3a450909095b20823a8443dd4d5
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-29 19:43:24 +0600

    Add files via upload

---
+++ BEE 3302.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,count=0,x;
+    cin>>n;
+    while(n-->0)
+    {
+
+        count++;
+        cin>>x;
+        cout<<"resposta "<<count<<": "<<x<<endl;
+
+    }
+    return 0;
+}
