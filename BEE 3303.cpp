8a8ff32efdf871ba998a28ee3782b50fc20cb7e2
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-01-29 20:26:34 +0600

    Add files via upload

---
+++ BEE 3303.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string name;
+    cin>>name;
+    if(name.size()>=10)
+        cout<<"palavrao"<<endl;
+    else
+        cout<<"palavrinha"<<endl;
+
+    return 0;
+
+}
