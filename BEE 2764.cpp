371122cbb80af8ef9d11386527dbe8bcfa3ea0e5
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-04-26 20:28:07 +0600

    Add files via upload

---
+++ BEE 2764.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string line;
+    getline(cin,line);
+    cout<<line[3]<<line[4]<<line[5]<<line[0]<<line[1]<<line[2]<<line[6]<<line[7]<<endl;
+    cout<<line[6]<<line[7]<<line[5]<<line[3]<<line[4]<<line[2]<<line[0]<<line[1]<<endl;
+    cout<<line[0]<<line[1]<<"-"<<line[3]<<line[4]<<"-"<<line[6]<<line[7]<<endl;
+    return 0;
+}
