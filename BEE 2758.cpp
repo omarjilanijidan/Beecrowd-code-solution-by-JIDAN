1aaffc271d2aa4ed8665a2705d4f4bfd6acf1da7
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2022-04-27 15:44:54 +0600

    Add files via upload

---
+++ BEE 2758.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    float a,b;
+    cin>>a>>b;
+    double x,y;
+    cin>>x>>y;
+
+
+    cout<<setprecision(6)<<fixed;
+    cout<<"A = "<<a<<", "<<"B = "<<b<<endl;
+    cout<<"C = "<<x<<", "<<"D = "<<y<<endl;
+
+    cout<<setprecision(1)<<fixed;
+    cout<<"A = "<<a<<", "<<"B = "<<b<<endl;
+    cout<<"C = "<<x<<", "<<"D = "<<y<<endl;
+    cout<<setprecision(2)<<fixed;
+    cout<<"A = "<<a<<", "<<"B = "<<b<<endl;
+    cout<<"C = "<<x<<", "<<"D = "<<y<<endl;
+    cout<<setprecision(3)<<fixed;
+    cout<<"A = "<<a<<", "<<"B = "<<b<<endl;
+    cout<<"C = "<<x<<", "<<"D = "<<y<<endl;
+
+
+    cout<<setprecision(3)<<scientific;
+    cout<<setiosflags(ios::uppercase);
+    cout<<"A = "<<a<<", "<<"B = "<<b<<endl;
+    cout<<"C = "<<x<<", "<<"D = "<<y<<endl;
+
+
+    cout<<setprecision(0)<<fixed;
+    cout<<"A = "<<a<<", "<<"B = "<<b<<endl;
+    cout<<"C = "<<x<<", "<<"D = "<<y<<endl;
+
+
+    return 0;
+}
+
