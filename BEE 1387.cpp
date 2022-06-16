1017e22056d39e40104c5f69c6996a7e22839abe
MD Omar Jilani Jidan <66664781+omarjilanijidan@users.noreply.github.com>
2022-06-16 12:20:05 +0600

    Add files via upload

---
+++ BEE 1387.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    while(cin>>x>>y)
+    {
+        if(x==0 && y==0)
+            break;
+        else
+            cout<<x+y<<endl;
+    }
+    return 0;
+}
