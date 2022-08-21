c84f73e8d4e04308c5bd1827c2b042ad9a8bded1
MD Omar Jilani Jidan <66664781+omarjilanijidan@users.noreply.github.com>
2022-08-21 22:20:51 +0600

    Add files via upload

---
+++ BEE 2926.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string c="a";
+    cin>>n;
+    for(int i=1;i<n;i++)
+    {
+
+        c+='a';
+    }
+    cout<<"Ent"<<c<<"o eh N"<<c<<"t"<<c<<"l!"<<endl;
+    return 0;
+}
