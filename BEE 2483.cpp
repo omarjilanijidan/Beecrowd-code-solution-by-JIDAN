b4474f53010ba012b62caa0dcb49b7e545abdbb5
MD Omar Jilani Jidan <66664781+omarjilanijidan@users.noreply.github.com>
2022-08-23 00:43:33 +0600

    Add files via upload

---
+++ BEE 2483.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    cin>>x;
+    string k,s="a";
+    for(int i=0;i<x;i++)
+        k+=s;
+    cout<<"Feliz nat"<<k<<"l!"<<endl;
+    return 0;
+}
