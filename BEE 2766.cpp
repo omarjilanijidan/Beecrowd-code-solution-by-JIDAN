ddb67ac9f21c213b2ce5b4f3ac07ed5a75052182
MD Omar Jilani Jidan <66664781+omarjilanijidan@users.noreply.github.com>
2022-08-23 00:28:46 +0600

    Add files via upload

---
+++ BEE 2766.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<string>v;
+    for(int i=0;i<10;i++)
+    {
+        string s;
+        cin>>s;
+        v.push_back(s);
+    }
+    cout<<v[2]<<endl;
+    cout<<v[6]<<endl;
+    cout<<v[8]<<endl;
+    return 0;
+
+}
